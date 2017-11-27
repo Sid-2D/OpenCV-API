cd525b6fcec9b16941b3b69b1bec3dbc0ac8dc6a
Sid <sid6327@gmail.com>
2017-11-27 23:40:51 +0530

    Adding new file for extracting digits from grid.

---
+++ ImageProcessing/gridDigits.cpp
@@ -0,0 +1,41 @@
+// Get individual blocks from processed image of gridDetection.cpp
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+	Mat img = imread("../Media/grid.jpg");
+	Mat scaled;
+	resize(img, scaled, Size(), 0.5, 0.28, CV_INTER_LINEAR);
+	namedWindow("Grid");
+	imshow("Grid", scaled);
+	// Start extraction
+	int numberHeight = scaled.size().height / 3;
+	int numberWidth = scaled.size().width / 9;
+	vector<Mat> numbers;
+	int totalNumbers = 0;
+	int startX = 0, startY = 0;
+	while (totalNumbers < 27) {
+		Rect rect = Rect(startX, startY, numberWidth, numberHeight);
+		totalNumbers++;
+		if (totalNumbers % 9 == 0) {
+			startX = 0;
+			startY += numberHeight; 
+		} else {
+			startX += numberWidth;
+		}
+		numbers.push_back(Mat(scaled, rect));
+	}
+	for (int i = 0; i < numbers.size(); i++) {
+		namedWindow("Numbers", CV_WINDOW_AUTOSIZE);
+		imshow("Numbers", numbers[i]);
+		waitKey(0);
+	}
+	return 0;
+}
